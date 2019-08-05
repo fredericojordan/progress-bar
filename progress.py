210086d8348dd3580c245e3a5b6aac334a1b3886
Frederico Jordan <fredericojordan@gmail.com>
2019-08-05 02:36:35 -0300

    Adding query params

--- progress.py
+++ progress.py
@@ -1,20 +1,55 @@
 import os
 
-from flask import Flask, make_response, render_template
+from flask import Flask, make_response, render_template, request
 
 app = Flask(__name__)
 app.config["SECRET_KEY"] = os.environ.get("SECRET_KEY", "secret_l801#+#a&^1mz)_p&qyq51j51@20_74c-xi%&i)b*u_dt^2=2key")
 
 
+def get_progress_color(progress, scale):
+    ratio = progress/scale
+
+    if ratio < 0.3:
+        return "#d9534f"
+    if ratio < 0.7:
+        return "#f0ad4e"
+
+    return "#5cb85c"
+
+
+def get_template_fields(progress):
+    title = request.args.get("title")
+
+    template_fields = {
+        "title": title,
+        "title_width": 10 + 6*len(title) if title else 0,
+        "scale": 100,
+        "progress": progress,
+        "progress_width": 60 if title else 90,
+        "suffix": request.args.get("suffix", "%"),
+    }
+
+    try:
+        template_fields["progress_width"] = int(request.args.get("width"))
+    except:
+        pass
+
+    try:
+        template_fields["scale"] = int(request.args.get("scale"))
+    except:
+        pass
+
+    template_fields["color"] = get_progress_color(progress, template_fields["scale"])
+
+    return template_fields
+
+
 @app.route("/<int:progress>/")
 def getProgressSVG(progress):
-    color = "#5cb85c"
-    if progress < 70:
-        color = "#f0ad4e"
-    if progress < 30:
-        color = "#d9534f"
+    template_fields = get_template_fields(progress)
 
-    template = render_template("progress.svg", width=90, progress=progress, color=color)
+    template = render_template("progress.svg", **template_fields)
+
     response = make_response(template)
     response.headers["Content-Type"] = "image/svg+xml"
     return response
