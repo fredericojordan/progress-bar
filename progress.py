a7f79135e2ba55debafc1867dadf8beffc396713
Frederico Jordan <fredericojordan@gmail.com>
2019-08-04 21:03:48 -0300

    Adding port and host

--- progress.py
+++ progress.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template
+
 app = Flask(__name__)
 
 
@@ -14,4 +15,4 @@ def getProgressSVG(progress):
 
 
 if __name__ == "__main__":
-    app.run()
+    app.run(host="0.0.0.0", port=80)
